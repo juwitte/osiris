c99b61fc98f3b8058b37278c191004628440190f
JKoblitz <julia.koblitz@osiris-solutions.de>
2024-11-03 13:28:09 +0100

    remove edit buttons if no permisson #35

--- pages/topics/topics.php
+++ pages/topics/topics.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The overview of all topics
  * Created in cooperation with bicc
@@ -14,7 +15,7 @@
  * @license     MIT
  */
 
- $topics  = $osiris->topics->find();
+$topics  = $osiris->topics->find();
 ?>
 
 
@@ -23,22 +24,26 @@
     <?= lang('Research Topics', 'Forschungsbereiche') ?>
 </h1>
 
-<a href="<?=ROOTPATH?>/topics/new"><?=lang('Add new topic', 'Neuen Bereich hinzufügen')?></a>
+<?php if ($Settings->hasPermission('topics.edit')) { ?>
+    <a href="<?= ROOTPATH ?>/topics/new"><?= lang('Add new topic', 'Neuen Bereich hinzufügen') ?></a>
+<?php } ?>
 
 <div id="topics">
     <?php foreach ($topics as $topic) { ?>
-        <div class="box padded topic" style="--topic-color: <?=$topic['color'] ?? '#333333'?>">
+        <div class="box padded topic" style="--topic-color: <?= $topic['color'] ?? '#333333' ?>">
             <h4 class="title">
                 <span class="topic-icon"></span>
-                <a href="<?=ROOTPATH?>/topics/view/<?=$topic['_id']?>" class="colorless"><?= $topic['name'] ?></a>
+                <a href="<?= ROOTPATH ?>/topics/view/<?= $topic['_id'] ?>" class="colorless"><?= $topic['name'] ?></a>
             </h4>
             <p class="font-size-12 text-muted">
                 <?= get_preview(lang($topic['description'], $topic['description_de'] ?? null), 400) ?>
             </p>
-            <a class="btn" href="<?=ROOTPATH?>/topics/edit/<?=$topic['_id']?>">
-                <i class="ph ph-edit"></i>
-                <?=lang('Edit', 'Bearbeiten')?>
-            </a>
+            <?php if ($Settings->hasPermission('topics.edit')) { ?>
+                <a class="btn" href="<?= ROOTPATH ?>/topics/edit/<?= $topic['_id'] ?>">
+                    <i class="ph ph-edit"></i>
+                    <?= lang('Edit', 'Bearbeiten') ?>
+                </a>
+            <?php } ?>
         </div>
     <?php } ?>
 </div>
